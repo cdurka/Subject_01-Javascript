52bd7e3d52d68a78a2a494a332748d6ac58415fc
cdurka <durkachinnasamy@gmail.com>
2024-06-28 21:38:17 +0530

    Updated a console log message and added a second log

---
+++ 01_JS_Basics/Ch_01_pgm_01.js
@@ -0,0 +1,9 @@
+//Changed the text from "Hello World!" to "Hello Universe!"
+
+console.log("Hello Universe!");
+
+//My additional console.log statement
+
+console.log("This is my second log message!");
+
+//Added a new console.log statement
