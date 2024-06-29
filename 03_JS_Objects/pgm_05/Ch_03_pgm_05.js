804e397972e079a688e8f3c8c7514974dfd3f9d2
cdurka <durkachinnasamy@gmail.com>
2024-06-29 22:30:48 +0530

    object with single property

---
+++ 03_JS_Objects/pgm_05/Ch_03_pgm_05.js
@@ -0,0 +1,17 @@
+var book;
+book = {
+    title: "The Hobbit"
+};
+
+//log book to console
+console.log(book);
+
+//second book
+var book2 = {
+    title: "1984"
+};
+
+//log book2 to console
+console.log(book2);
+
+//type book , press enter = book object should appear ( in console tab)
