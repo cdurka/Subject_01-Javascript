1c5f8ddc2a06568f2928a9778d4a001dece1602c
cdurka <durkachinnasamy@gmail.com>
2024-06-29 22:59:18 +0530

    used dot notation to access property values

---
+++ 03_JS_Objects/pgm_08/Ch_03_pgm_08.js
@@ -0,0 +1,23 @@
+var book;
+book = {
+    title     : "The Hobbit",
+    author    : "J. R. R. Tolkien",
+    published : 1937
+};
+console.log(book.title);
+console.log(book.author);
+
+//log published property
+console.log(book.published);
+
+//add second book
+var book2 = {
+    title     : "Northern Lights",
+    author    : "Philip Pullman",
+    published : 1995
+};
+
+//log its details
+console.log(book2.title);
+console.log(book2.author);
+console.log(book2.published);
