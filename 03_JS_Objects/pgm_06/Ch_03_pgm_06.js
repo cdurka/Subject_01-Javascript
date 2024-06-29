198b13986e303729bfd8c73dae340d1b547c1044
cdurka <durkachinnasamy@gmail.com>
2024-06-29 22:41:29 +0530

    used variable as a property value

---
+++ 03_JS_Objects/pgm_06/Ch_03_pgm_06.js
@@ -0,0 +1,15 @@
+var book;
+var bookName;
+ bookName = "The Adventures of Tom Sawyer";
+ book = {
+    title: bookName
+ };
+
+ //run the program
+ console.log(book);
+
+ //book object should appear with same title and value in console 
+ //changing value of bookNsme variable
+ bookName = "Harry Potter and the Philosopher's Stone";
+ book.title = bookName;
+ console.log(book);
