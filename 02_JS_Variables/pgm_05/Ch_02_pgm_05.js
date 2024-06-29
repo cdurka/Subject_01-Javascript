3f20433a344cceaa016e4b02716374ed382a1c0e
cdurka <durkachinnasamy@gmail.com>
2024-06-29 20:18:52 +0530

    assigning text to variables

---
+++ 02_JS_Variables/pgm_05/Ch_02_pgm_05.js
@@ -0,0 +1,18 @@
+var message;
+message = "Hello World";
+console.log(message);
+
+message = "Congratulations! Your tweet has won a prize...";
+console.log(message);
+
+//assign message a third value and display
+message = "This is a new message!";
+console.log(message);
+
+//split first message into two 
+message = "Hello " + "World!";
+console.log(message);
+
+//split second message into three
+message = "Congratulations! " + "Your tweet " + "has won a prize...";
+console.log(message);
