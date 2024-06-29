81d4216dd81a89e01ab1a7eceb115a5a336f7891
cdurka <durkachinnasamy@gmail.com>
2024-06-29 20:50:35 +0530

    using variable's current value to set its new value

---
+++ 02_JS_Variables/pgm_08/Ch_02_pgm_08.js
@@ -0,0 +1,19 @@
+var score;
+score = 100;
+console.log("Your score was " + score);
+console.log("Great splat!!!" );
+score = score + 50;
+console.log("New score: " + score);
+console.log("Way to go!");
+
+//tell the player of a successful splat
+console.log("You splatted a kumquat!");
+
+//add 100 to player"s score
+score = score + 100;
+
+//show new score
+console.log("New score: " + score);
+
+//display message of congratulations
+console.log("Congratulations! You're doing great!");
