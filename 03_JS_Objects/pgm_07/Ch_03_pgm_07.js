17f024f083ee5d2535576ee5033b89c0006bda4a
cdurka <durkachinnasamy@gmail.com>
2024-06-29 22:49:59 +0530

    objects with multiple properties

---
+++ 03_JS_Objects/pgm_07/Ch_03_pgm_07.js
@@ -0,0 +1,17 @@
+var book1;
+var book2;
+book1 = {
+    title     : "The Hobbit",
+    author    : "J. R. R. Tolkien",
+    published : 1937 //added third property
+};
+
+book2 = {
+    title     : "Northern Lights",
+    author    : "Philip Pullman",
+    published : 1995 //added third property
+};
+
+//log both objects to console
+console.log(book1);
+console.log(book2);
