7e2bd202d91aab565df2812ab02d637cd2c93ae5
cdurka <durkachinnasamy@gmail.com>
2024-06-29 20:09:31 +0530

    alter value and display

---
+++ 02_JS_Variables/pgm_04/Ch_02_pgm_04.js
@@ -0,0 +1,23 @@
+var score;
+score=100;
+console.log(score);
+
+score=150;
+console.log(score);
+
+//change value
+score=200;
+console.log(score);
+
+//second var
+var score2;
+//assign value
+score2=300;
+//display
+console.log(score2);
+
+//add new line of code to alter value of var
+score2=350;
+
+//add new line of code to display new value
+console.log(score2);
