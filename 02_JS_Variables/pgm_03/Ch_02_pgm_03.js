ed50d15815397fc65b90a9bb20d492d860da186a
cdurka <durkachinnasamy@gmail.com>
2024-06-29 20:00:58 +0530

    created second var and assigned value

---
+++ 02_JS_Variables/pgm_03/Ch_02_pgm_03.js
@@ -0,0 +1,16 @@
+var score;
+score = 100;
+console.log(score);
+
+//Change the value assigned to score
+score = 200;
+console.log(score);
+
+//second variable
+var score2;
+
+//value assigning
+score2=300;
+
+//display var
+console.log(score2);
