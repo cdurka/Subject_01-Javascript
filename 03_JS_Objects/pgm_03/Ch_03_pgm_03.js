28d51bd545553d54feee7035423276a9a5d82eb9
cdurka <durkachinnasamy@gmail.com>
2024-06-29 22:17:24 +0530

    created a book as object

---
+++ 03_JS_Objects/pgm_03/Ch_03_pgm_03.js
@@ -0,0 +1,20 @@
+var book;
+book = {
+    title: "The Hobbit",
+    author: "J. R. R. Tolkien",
+    published: 1937
+};
+  
+//changing title of book
+book.title = "The Hobbit, or There and Back Again";
+console.log(book);
+
+//creating a book2 object
+var book2 = {
+    title: "1984",
+    author: "George Orwell",
+    published: 1949
+};
+
+//log book2 to console
+console.log(book2);
