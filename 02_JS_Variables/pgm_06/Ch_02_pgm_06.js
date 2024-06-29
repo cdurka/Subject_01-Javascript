9b5bff35ede2e1713b5994f3b0aced265fc76212
cdurka <durkachinnasamy@gmail.com>
2024-06-29 20:28:45 +0530

    combined variable and string

---
+++ 02_JS_Variables/pgm_06/Ch_02_pgm_06.js
@@ -0,0 +1,15 @@
+var playerName;
+var locationNme;
+
+playerName = "Lionel Messi";
+locationNme = "Argentina";
+
+console.log(playerName + " is in " + locationNme);
+
+//declare score var
+var score;
+//assign value
+score = 100;
+
+//extra message on console by combining variables and strings
+console.log(playerName + " has a score of " + score);
