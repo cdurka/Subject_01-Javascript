944848d451ccab31c97c7c14e0382e164fab5612
cdurka <durkachinnasamy@gmail.com>
2024-06-29 21:19:09 +0530

    valid and invalid variable names

---
+++ 02_JS_Variables/pgm_09/Ch_02_pgm_09.js
@@ -0,0 +1,13 @@
+var thisIsFine;
+var $noProblemHere;
+var _underscore56;
+var StarWithCapital;
+var z5;
+
+/*delete the second block (commented)
+var 999; //invalid:cannot start with number
+var 39Steps; //invalid:cannot start with number
+var &nope; //invalid:cannot use special characters 
+var single words only; //invalid:cannot contain spaces
+var yield; //invalid:'yield' is a keyword
+*/
