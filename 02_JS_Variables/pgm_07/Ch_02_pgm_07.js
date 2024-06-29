069137e03df98704fff976114d3275683dd7d018
cdurka <durkachinnasamy@gmail.com>
2024-06-29 20:39:47 +0530

    declaring & assigning in one step

---
+++ 02_JS_Variables/pgm_07/Ch_02_pgm_07.js
@@ -0,0 +1,16 @@
+var playerName = "Kandra";
+var locationName = "The Dungeon of Doom";
+
+console.log(playerName + " is in " + locationName);
+
+// declare health variable and assign value
+var health = 50;
+
+//declare message variable
+var message;
+
+//assign message a value by joining variables
+message = playerName + " has health " + health + " and is in " + locationName;
+
+//replace current console.log ith new one
+console.log(message);
