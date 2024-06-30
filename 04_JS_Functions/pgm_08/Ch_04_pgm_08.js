6980252d33a713bafe93cd6fbef03527ffa7644a
cdurka <durkachinnasamy@gmail.com>
2024-06-30 16:46:07 +0530

    displaying menu

---
+++ 04_JS_Functions/pgm_08/Ch_04_pgm_08.js
@@ -0,0 +1,22 @@
+var displayMenu;
+
+displayMenu = function () {
+    console.log("Please choose an option:");
+    console.log("(1) Print log");
+    console.log("(2) Upload file");
+    console.log("(3) Download file");//added option
+    console.log("(4) Delete file");//added option
+    console.log("(9) Quit");
+};
+
+displayMenu();
+var displayMyMenu;
+displayMyMenu = function () {
+    console.log("My Custom Menu:");
+    console.log("(1) Start new game");
+    console.log("(2) Load game");
+    console.log("(3) Save game");
+    console.log("(4) View high scores");
+    console.log("(9) Exit");
+};
+displayMyMenu();
