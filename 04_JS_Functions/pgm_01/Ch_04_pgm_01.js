e1fd7d0c3823e4a4cab7e4ebe721e4278b81af87
cdurka <durkachinnasamy@gmail.com>
2024-06-30 12:48:46 +0530

    displaying an object's properties on console

---
+++ 04_JS_Functions/pgm_01/Ch_04_pgm_01.js
@@ -0,0 +1,40 @@
+var movie1;
+movie1 = {
+    title: "Inside Out",
+    actors: "Amy Poehler, Bill Hader",
+    directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+console.log("Movie information for " + movie1.title);
+console.log("-------------------------------------");
+console.log("Actors: " + movie1.actors);
+console.log("Directors: " + movie1.directors);
+console.log("-------------------------------------");
+
+//add second movie and display
+var movie2 = {
+    title: "Inception",
+    actors: "Leonardo DiCaprio, Joseph Gordon-Levitt",
+    directors: "Christopher Nolan"
+};
+console.log("Movie information for " + movie2.title);
+console.log("-------------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("-------------------------------------");
+
+//create an object to represent a blog post
+var blogPost = {
+    title: "My Amazing Journey",
+    author: "John Doe",
+    created: "2024-06-29",
+    content: "Today,  I embarked on an incredible adventure..."
+};
+
+//code to display about blog post
+console.log("Blog Post Information");
+console.log("------------------------------------");
+console.log("Title: " + blogPost.title);
+console.log("Author: " + blogPost.author);
+console.log("Created on: " + blogPost.created);
+console.log("Content: " + blogPost.content);
+console.log("------------------------------------");
