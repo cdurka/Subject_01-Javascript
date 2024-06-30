596532a3a9d1ec717db14aa0ca9e4725e76ea2b9
cdurka <durkachinnasamy@gmail.com>
2024-06-30 12:10:41 +0530

    A question and answer for a quiz

---
+++ 03_JS_Objects/pgm_14/Ch_03_pgm_14.js
@@ -0,0 +1,16 @@
+var questionAndAnswer = {
+    question: "What is the capital of France?",
+    answer1: "Bordeaux",
+    answer2: "Lyon",
+    answer3: "Paris",
+    answer4: "Brussels",
+    correctAnswer: "Paris",
+    marksForQuestion: 2
+};
+
+//use console.log to present
+console.log("Question: " + questionAndAnswer.question);
+console.log("1. " + questionAndAnswer.answer1);
+console.log("2. " + questionAndAnswer.answer2);
+console.log("3. " + questionAndAnswer.answer3);
+console.log("4. " + questionAndAnswer.answer4);
