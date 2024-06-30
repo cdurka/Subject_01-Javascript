b87bafaf9daa6b266650572ff49c54949e429e94
cdurka <durkachinnasamy@gmail.com>
2024-06-30 11:54:32 +0530

    Display properties of post

---
+++ 03_JS_Objects/pgm_12/Ch_03_pgm_12.js
@@ -0,0 +1,25 @@
+var post = {
+    id    : 1,
+    title  : "My Crazy Space Adventure" ,
+    author : "Philae" ,
+    created : "2015-06-21",
+    body :"You will not believe where i just woke up!! Only on a comet..."
+};
+//display some of properties
+console.log("Post Title: " + post.title);
+console.log("Autor: " + post.author);
+console.log("Created on: " + post.created);
+
+//second post
+var post2 = {
+    id : 2,
+    title : "Journey to the Center of the Earth",
+    author : "Jules Verne",
+    created : "1864-11-25",
+    body : "An amazing adventure to the center of our planet."
+};
+
+//display properties of second post
+console.log("Post Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Created on: " + post2.created);
