59a066f7cedac81504449da2bff54c1c9d71295d
cdurka <durkachinnasamy@gmail.com>
2024-06-30 12:01:37 +0530

    location for a weather app

---
+++ 03_JS_Objects/pgm_13/Ch_03_pgm_13.js
@@ -0,0 +1,12 @@
+var geolocation = {
+    "city"       : "San Francisco",
+    "state"      : "CA",
+    "country"    : "US",
+    "zip"        : "94101",
+    "latitude"   : 37.775,
+    "longtitude" : -122.418,
+    "elevation"  : 47.000
+};
+
+//run the program
+console.log(geolocation);
