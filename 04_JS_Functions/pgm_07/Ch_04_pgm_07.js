67554f5b370f90d2bfe96c5c33f8d04b9ddcbf25
cdurka <durkachinnasamy@gmail.com>
2024-06-30 16:35:20 +0530

    updated third variable to show new calculation

---
+++ 04_JS_Functions/pgm_07/Ch_04_pgm_07.js
@@ -0,0 +1,15 @@
+//change number1 and number2
+var number1 = 66;
+var number2 = 1000;
+//third variable
+var number3 = 34;
+var findTotal;
+
+findTotal = function () {
+    result = number1 + number2 + number3; //update to add three numbers
+};
+
+findTotal();
+
+console.log(number1 + " + " + number2 + " + " + number3 +" = " + result );
+//update to show the new calculation
