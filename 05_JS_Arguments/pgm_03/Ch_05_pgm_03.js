f19e4fb45abc0b12481e7bf7bf8e335cf4567dc9
cdurka <durkachinnasamy@gmail.com>
2024-07-01 19:28:56 +0530

    passing information to a function

---
+++ 05_JS_Arguments/pgm_03/Ch_05_pgm_03.js
@@ -0,0 +1,13 @@
+var showMessage;
+
+//change showMessage function,add some extra text
+showMessage = function (message) {
+    console.log("The message is: " + message + ". Have a great day!");
+};
+
+//change the message in parenthesis
+showMessage("Hello, universe!");
+
+//add two more calls to the showMessage function with different arguments
+showMessage("JavaScript is awesome!");
+showMessage("Keep on coding!");
