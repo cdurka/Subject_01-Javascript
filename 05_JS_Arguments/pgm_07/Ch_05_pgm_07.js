347cd4be0cb1e6b83cafab7f24cf8eacb6631777
cdurka <durkachinnasamy@gmail.com>
2024-07-02 20:10:14 +0530

    displaying a player's name

---
+++ 05_JS_Arguments/pgm_07/Ch_05_pgm_07.js
@@ -0,0 +1,10 @@
+var showPlayerName;
+
+showPlayerName = function (playerName)
+{
+    console.log("The player's name is " + playerName);
+    console.log("The name has " + playerName.length + " letters.");
+};
+
+showPlayerName("Kandra");
+showPlayerName("Dax");
