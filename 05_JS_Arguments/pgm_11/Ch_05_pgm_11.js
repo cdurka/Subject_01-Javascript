42b10db55c2267d6a4f9df103ef93b14b8968a14
cdurka <durkachinnasamy@gmail.com>
2024-07-04 10:30:25 +0530

    displaying a player's location (using array)

---
+++ 05_JS_Arguments/pgm_11/Ch_05_pgm_11.js
@@ -0,0 +1,32 @@
+//changing playerName to playerName[0] 
+
+var showPlayerPlace;
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName[0] + " is in " + playerPlace);
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");
+
+//changing the number in the bracket to 1
+
+var showPlayerPlace;
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName[1] + " is in " + playerPlace);
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");
+
+//changing the number to 3
+
+var showPlayerPlace;
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName[3] + " is in " + playerPlace);
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");
