d8858e0dc98f4843799595aefb2c0ee3ea18344f
cdurka <durkachinnasamy@gmail.com>
2024-07-04 20:56:44 +0530

    getMessage

---
+++ 06_JS-Return_values/pgm_01/Ch_06_pgm_01.js
@@ -0,0 +1,14 @@
+var getMessage;
+var getMyMessage;
+var response;
+
+getMessage = function () {
+    return "I'm going on an adventure!";
+};
+
+getMyMessage = function () {
+    return "Learning Javascript is fun!";
+};
+
+response = getMessage();
+console.log(response);
