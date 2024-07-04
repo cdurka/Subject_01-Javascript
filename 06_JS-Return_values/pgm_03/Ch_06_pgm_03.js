5397f08022696fe34b097ed894d5061be24b4419
cdurka <durkachinnasamy@gmail.com>
2024-07-04 21:12:31 +0530

    replace function

---
+++ 06_JS-Return_values/pgm_03/Ch_06_pgm_03.js
@@ -0,0 +1,11 @@
+var getHelloTo;
+
+getHelloTo = function (name) {
+    var template;
+    template = "Hello to {{name}}";
+    template = template.replace("{{name}}", name);
+    return template;
+};
+
+console.log(getHelloTo("Kandra"));
+console.log(getHelloTo("Dax"));
