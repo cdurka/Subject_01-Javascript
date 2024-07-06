0c6a6d002661b55dfcb5498232b063e5e789e331
cdurka <durkachinnasamy@gmail.com>
2024-07-06 22:30:19 +0530

    Index

---
+++ 07_JS_Object_Arguments/pgm_09/Ch_07_pgm_09.js
@@ -0,0 +1,13 @@
+var message = "We choose to go to the Moon!";
+
+var charIndex = message.indexOf("M");
+console.log(message.substr(charIndex,3));
+
+var goIndex = message.indexOf("go");
+console.log(goIndex);
+
+var chooseIndex = message.indexOf("choose");
+console.log(message.substr(chooseIndex,6));
+
+var ooIndex = message.lastIndexOf("oo");
+console.log(ooIndex);
